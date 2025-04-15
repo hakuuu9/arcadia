9c14920738f65049752447fdbb4fff7114550b95
hakuuu9 <hakuunlyy@gmail.com>
2025-04-16 00:04:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 ROLE_ID = 1361732154584858724  # Replace with your role ID
-INVITE_LINK = ".gg/arcadia"  # Replace with your vanity link
+INVITE_LINK = ".gg/arcadiasolana"  # Replace with your vanity link
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Securely gets the token from Render's environment variable
