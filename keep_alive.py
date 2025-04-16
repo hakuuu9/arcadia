0aa8a0a5b498474ba66e91635113977d194040da
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 00:32:52 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!", 200
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
+    return "I'm alive!"
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080}).start()
