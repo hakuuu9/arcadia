97a35cadc6bf5841a215277179c46d797d90f4ae
hakuuu9 <hakuunlyy@gmail.com>
2025-04-16 12:39:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 ROLE_ID = 1361732154584858724  # Replace with your role ID
 INVITE_LINK = ".gg/arcadiasolana"  # Replace with your vanity link
-LOG_CHANNEL_ID = 1357655631544320085  # Replace with the ID of your logs channel
+LOG_CHANNEL_ID = 1357671759511556216  # Replace with the ID of your logs channel
