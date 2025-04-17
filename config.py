d774ca79b69e7ccc14ab86c28b7807e2c08d909f
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 10:43:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.GALfGf.DRA9m24yIOi9qDzhqCfWm6xsfRcaN1DY50sZ5s"
+GUILD_ID = 1259666579214569552
 ROLE_ID = 1361732154584858724  # Replace with your role ID
 INVITE_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
 LOG_CHANNEL_ID = 1357671759511556216  # Replace with the ID of your logs channel
