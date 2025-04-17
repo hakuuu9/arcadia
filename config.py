61ecc91edfca28bc6c940c93697073ed70f9e1d1
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 18:46:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
+TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
 ROLE_ID = 1361732154584858724  # Replace with your role ID
 INVITE_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
 LOG_CHANNEL_ID = 1357671759511556216  # Replace with the ID of your logs channel
