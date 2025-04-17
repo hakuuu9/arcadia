d5afe3ed0870359b2b586263f4a4e3922f918502
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 18:19:47 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord import utils
 import config
 
 intents = discord.Intents.default()
-intents.members = True  # We need this to track member updates (e.g., status changes)
+intents.members = True  # To track member updates (like status changes)
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
@@ -13,29 +13,29 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
-# Check if a user's status contains a vanity link
+# Check if the user's status contains the vanity link
 async def check_vanity_link_in_status(user):
-    for link, role_name in config.VANITY_LINKS.items():
-        if link in user.activity and role_name:
-            role = utils.get(user.guild.roles, name=role_name)
-            if role:
-                if role not in user.roles:
-                    await user.add_roles(role)
-                    print(f"Assigned {role_name} to {user.name}")
-        else:
-            # If the vanity link is not in the status, remove the role if it exists
-            role = utils.get(user.guild.roles, name=role_name)
-            if role and role in user.roles:
-                await user.remove_roles(role)
-                print(f"Removed {role_name} from {user.name}")
+    if config.VANITY_LINK in user.activity:
+        # User has the vanity link in their status, so assign the role
+        role = utils.get(user.guild.roles, id=config.ROLE_ID)
+        if role and role not in user.roles:
+            await user.add_roles(role)
+            log_channel = bot.get_channel(config.LOG_CHANNEL_ID)
+            await log_channel.send(f"Assigned {role.name} role to {user.name} for having the vanity link in their status.")
+    else:
+        # User doesn't have the vanity link in their status, remove the role
+        role = utils.get(user.guild.roles, id=config.ROLE_ID)
+        if role and role in user.roles:
+            await user.remove_roles(role)
+            log_channel = bot.get_channel(config.LOG_CHANNEL_ID)
+            await log_channel.send(f"Removed {role.name} role from {user.name} for removing the vanity link from their status.")
 
 # Event that runs when a member's status changes
 @bot.event
 async def on_member_update(before, after):
-    # Check if the user's status has changed
     if before.activity != after.activity:
         await check_vanity_link_in_status(after)
 
 # Run the bot
 if __name__ == "__main__":
-    bot.run(config.DISCORD_TOKEN)
+    bot.run(config.TOKEN)
