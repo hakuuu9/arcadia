eb0e1c20f30b58893ee1affdf7d980cb4bdb65c0
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 17:38:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,16 @@
 import discord
 from discord.ext import commands
-from config import TOKEN
-import os
+from keep_alive import run  # Import the function to run Flask
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-# Load the cogs
-bot.load_extension("cogs.ship")
-bot.load_extension("cogs.remind")
-bot.load_extension("cogs.choose")
-bot.load_extension("cogs.afk")
-bot.load_extension("cogs.vanity")
+# Start the Flask app in the background
+import threading
+threading.Thread(target=run).start()
 
-bot.run(TOKEN)
+bot.run('YOUR_BOT_TOKEN')
