b00f2c81f94653cc0ff2b915b0bafbd7f18972a2
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 18:42:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-
-# GUILD_ID = 1259666579214569552
-# LOG_CHANNEL_ID = 1357671759511556216  # Replace with the ID of your logs channel
-
-# 🔧 Configuration File
-
-ROLE_ID = 1361732154584858724  # Replace with your actual role ID
-INVITE_LINK = "discord.gg/arcadiasolana"  # Your server vanity link
-BOT_TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"  # Your bot token
+BOT_TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
+ROLE_ID = 1361732154584858724  # Replace with your role ID
+INVITE_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
+LOG_CHANNEL_ID = 1357671759511556216  # Replace with the ID of your logs channel
