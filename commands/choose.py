0808a417afb0e607df2594c1c76b97a1daf859cb
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 17:19:02 +0800

    Update choose.py

--- commands/choose.py
+++ commands/choose.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import random
 
@@ -5,15 +6,24 @@ class Choose(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def choose(self, ctx, *, choices):
-        options = [option.strip() for option in choices.split(",") if option.strip()]
-        if len(options) < 2:
-            await ctx.send("Please provide at least two options separated by commas.")
-        else:
-            choice = random.choice(options)
-            await ctx.send(f"I choose: **{choice}**")
+    @commands.command(name="choose")
+    async def choose(self, ctx, *, options: str):
+        """
+        Command that allows the bot to choose randomly from a list of options.
+        Users provide a string of options separated by commas.
+        Example usage: $choose option1, option2, option3
+        """
+        # Split the options by commas and strip any extra spaces
+        options_list = [option.strip() for option in options.split(',')]
 
+        if len(options_list) < 2:
+            await ctx.send("Please provide at least two options to choose from!")
+            return
+        
+        chosen_option = random.choice(options_list)
+        await ctx.send(f"I choose: **{chosen_option}**")
+
+# Setup the cog
 def setup(bot):
     bot.add_cog(Choose(bot))
-  
+    
