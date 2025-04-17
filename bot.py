013cc5d316527d837919e7acb405d7eda5bffc98
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 17:25:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
 import discord
 from discord.ext import commands
 from config import TOKEN
-from keep_alive import keep_alive
 import os
-import asyncio
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="$", intents=intents)
-
-# Load commands from commands folder
-for filename in os.listdir("./commands"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"commands.{filename[:-3]}")
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-keep_alive()  # Starts Flask server
+# Load the cogs
+bot.load_extension("cogs.ship")
+bot.load_extension("cogs.remind")
+bot.load_extension("cogs.choose")
+bot.load_extension("cogs.afk")
+bot.load_extension("cogs.vanity")
+
 bot.run(TOKEN)
