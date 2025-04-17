eb0010652c7c4a5a3cfc2951986d6fb7b41e4f64
hakuuu9 <hakuunlyy@gmail.com>
2025-04-17 16:45:26 +0800

    Update ship.py

--- commands/ship.py
+++ commands/ship.py
@@ -1,1 +1,21 @@
+import random
+from discord.ext import commands
+import discord
 
+class Ship(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ship(self, ctx, user1: discord.Member, user2: discord.Member):
+        percent = random.randint(0, 100)
+        hearts = "❤️" * (percent // 20) or "💔"
+        nicknames = ["Lovebirds", "Twin Flames", "Sweethearts", "Power Couple", "Perfect Pair"]
+        description = f"**{user1.display_name}** 💞 **{user2.display_name}**\nCompatibility: **{percent}%** {hearts}"
+        if percent >= 50:
+            description += f"\nCouple Nickname: **{random.choice(nicknames)}**"
+        await ctx.send(description)
+
+def setup(bot):
+    bot.add_cog(Ship(bot))
+  
