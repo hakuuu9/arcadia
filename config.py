395eec4e6548f5f3c396baa259b5b222c3511821
hakuuu9 <hakuunlyy@gmail.com>
2025-04-18 08:21:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
-# GUILD_ID = 1259666579214569552
+TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
+GUILD_ID = 1259666579214569552
 
 ROLE_ID = 1361732154584858724  # Replace with your role ID
-INVITE_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
+VANITY_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
 LOG_CHANNEL_ID = 1357671759511556216  # Replace with the ID of your logs channel
