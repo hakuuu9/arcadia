83f37e185e365978665198116b6cad9119840947
hakuuu9 <hakuunlyy@gmail.com>
2025-04-20 15:02:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
 GUILD_ID = 1259666579214569552
-
+VANITY_IMAGE_URL = "https://i.imgur.com/3pHNmTH.png"
 ROLE_ID = 1361732154584858724  # Replace with your role ID
 VANITY_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
 LOG_CHANNEL_ID = 1352902957590380584  # Replace with the ID of your logs channel
