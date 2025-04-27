790fa08b973655ec40154b645fc039d4af6b1a09
hakuuu9 <hakuunlyy@gmail.com>
2025-04-27 09:16:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TOKEN = "MTM2MTY5NDg4MDkyNDk1ODk2OA.G7wLuK.onlixf-jWQlYATFX8b-ZPr4can56yyZVH-rVKY"
 GUILD_ID = 1259666579214569552
-VANITY_IMAGE_URL = "https://cdn.discordapp.com/attachments/1352902957590380584/1363435446993944576/image.png"
+VANITY_IMAGE_URL = "https://cdn.discordapp.com/attachments/1364905044239187989/1365858385605562408/Your_paragraph_text_20250427_091202_0000.jpg"
 ROLE_ID = 1361732154584858724  # Replace with your role ID
 VANITY_LINK = "discord.gg/arcadiasolana"  # Replace with your vanity link
 LOG_CHANNEL_ID = 1363396246663860356  # Replace with the ID of your logs channel
