b21dbad0d366c75d2a5e379076d5124b2186a5cd
hakuuu9 <hakuunlyy@gmail.com>
2025-05-05 01:32:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-
+TOKEN = os.environ["TOKEN"]
 GUILD_ID = 1259666579214569552
 VANITY_IMAGE_URL = "https://cdn.discordapp.com/attachments/1364905044239187989/1365858385605562408/Your_paragraph_text_20250427_091202_0000.jpg"
 ROLE_ID = 1361732154584858724  # Replace with your role ID
