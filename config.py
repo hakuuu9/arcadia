37a4bbe7d8a0b81e3c67ca4991ec9ffc6531d9c0
hakuuu9 <hakuunlyy@gmail.com>
2025-05-13 19:13:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ TOKEN = os.environ["TOKEN"]
 GUILD_ID = 1259666579214569552
 VANITY_IMAGE_URL = "https://cdn.discordapp.com/attachments/1364905044239187989/1365858385605562408/Your_paragraph_text_20250427_091202_0000.jpg"
 ROLE_ID = 1361732154584858724  # Replace with your role ID
-VANITY_LINK = ".gg/warcadia"  # Replace with your vanity link
+VANITY_LINK = "discord.gg/warcadia"  # Replace with your vanity link
 LOG_CHANNEL_ID = 1363396246663860356  # Replace with the ID of your logs channel
