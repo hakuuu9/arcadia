d3007b3fce4b2a5891dc0852750b3e0002bc95d8
hakuuu9 <hakuunlyy@gmail.com>
2025-05-18 04:30:16 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -245,64 +245,68 @@ async def createembed(ctx, *, content: str = None):
 
 
 
-
 @bot.command()
 async def role(ctx, member: discord.Member = None, *, role_input: str = None):
-    STAFF_ROLE_IDS = {1347181345922748456, 1359467025109618849}
-    LOG_CHANNEL_ID = 1364839238960549908
+STAFF_ROLE_NAME = "Moderator, Supreme Authority"
+STAFF_ROLE_ID = 1347181345922748456, 1359467025109618849
+LOG_CHANNEL_ID = 1364839238960549908
 
-    if not any(role.id in STAFF_ROLE_IDS for role in ctx.author.roles):
-        await ctx.send("❌ You don't have permission to use this command.")
-        return
+staff_role = discord.utils.get(ctx.guild.roles, name=STAFF_ROLE_NAME)  
 
-    if not member or not role_input:
-        await ctx.send("Usage: `$role @member @role`")
-        return
+if not staff_role or staff_role.id != STAFF_ROLE_ID:  
+    await ctx.send("❌ You don't have the required staff role.")  
+    return  
+if STAFF_ROLE_ID not in [role.id for role in ctx.author.roles]:  
+    await ctx.send("❌ You don't have permission to use this command.")  
+    return  
 
-    # Try to get the role
-    role = None
-    if role_input.isdigit():
-        role = ctx.guild.get_role(int(role_input))
-    elif role_input.startswith("<@&") and role_input.endswith(">"):
-        role_id = int(role_input[3:-1])
-        role = ctx.guild.get_role(role_id)
-    else:
-        role = discord.utils.find(lambda r: r.name.lower() == role_input.lower(), ctx.guild.roles)
+if not member or not role_input:  
+    await ctx.send("Usage: `$role @member @role`")  
+    return  
 
-    if not role:
-        await ctx.send("❌ Couldn't find that role.")
-        return
+role = None  
+if role_input.isdigit():  
+    role = ctx.guild.get_role(int(role_input))  
+elif role_input.startswith("<@&") and role_input.endswith(">"):  
+    role_id = int(role_input[3:-1])  
+    role = ctx.guild.get_role(role_id)  
+else:  
+    role = discord.utils.find(lambda r: r.name.lower() == role_input.lower(), ctx.guild.roles)  
 
-    granted_emoji = "<a:GC_Fire:1348482027447386116>"
-    revoked_emoji = "<a:calcifer:1348189333542404106>"
+if not role:  
+    await ctx.send("❌ Couldn't find that role.")  
+    return  
 
-    embed = discord.Embed(
-        color=discord.Color.blurple(),
-        timestamp=datetime.datetime.utcnow()
-    )
-    embed.set_thumbnail(url="https://i.imgur.com/JxsCfCe.gif")
-    embed.set_footer(text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.display_avatar.url)
+granted_emoji = "<a:GC_Fire:1348482027447386116>"  
+revoked_emoji = "<a:calcifer:1348189333542404106>"  
 
-    if role in member.roles:
-        await member.remove_roles(role)
-        embed.title = f"{revoked_emoji} Role Revoked"
-        embed.description = (
-            f"The role **{role.name}** has been revoked from {member.mention}.\n"
-            f"All permissions associated with this role have been removed."
-        )
-    else:
-        await member.add_roles(role)
-        embed.title = f"{granted_emoji} Role Granted"
-        embed.description = (
-            f"{member.mention} has been granted the **{role.name}** role.\n"
-            f"Relevant permissions are now active."
-        )
+embed = discord.Embed(  
+    color=discord.Color.blurple(),  
+    timestamp=discord.utils.utcnow()  
+)  
+embed.set_thumbnail(url="https://i.imgur.com/JxsCfCe.gif")  
+embed.set_footer(text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.display_avatar.url)  
 
-    await ctx.send(embed=embed)
+if role in member.roles:  
+    await member.remove_roles(role)  
+    embed.title = f"{revoked_emoji} Role Revoked"  
+    embed.description = (  
+        f"The role **{role.name}** has been revoked from {member.mention}.\n"  
+        f"All permissions associated with this role have been removed."  
+    )  
+else:  
+    await member.add_roles(role)  
+    embed.title = f"{granted_emoji} Role Granted"  
+    embed.description = (  
+        f"{member.mention} has been granted the **{role.name}** role.\n"  
+        f"Relevant permissions are now active."  
+    )  
 
-    log_channel = ctx.guild.get_channel(LOG_CHANNEL_ID)
-    if log_channel:
-        await log_channel.send(embed=embed)
+await ctx.send(embed=embed)  
+
+log_channel = ctx.guild.get_channel(LOG_CHANNEL_ID)  
+if log_channel:  
+    await log_channel.send(embed=embed)
 
 
 
